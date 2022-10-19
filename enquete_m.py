711faf468dac8b391887a05f905e025f6f6a2f11
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-10-19 12:46:05 -0400

    Create `enquete_m.py`

---
+++ enquete_m.py
@@ -0,0 +1,12 @@
+import names
+
+salle1 = "Salle 1"
+spectateurs1 = [names.get_full_name() for i in range(15)]
+
+salle2 = "Salle 2"
+spectateurs2 = [names.get_full_name() for i in range(10)]
+
+salle3 = "Salle 3"
+spectateurs3 = [names.get_full_name() for i in range(20)]
+
+trajet = [(91.68, 53.10), (81.52, 50.14), (43.68, 18.18)]
