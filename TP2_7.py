ff4dbc11b78d446b719ddf422dd658eecfc17234
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-10-19 13:37:36 -0400

    push du give up

--- TP2_7.py
+++ TP2_7.py
@@ -13,33 +13,61 @@ from enquete_m import *
 def main():
     pres12 = []  # liste des spectateurs présents dans les salles 1 et 2
     pres123 = []  # liste des spectateurs présents dans les salles 1, 2 et 3
+
+    # remplir la liste avec les noms des spectateurs présents dans les salles 1 et 2
     for spectateur in spectateurs1:
         pres12.append(spectateur)
     for spectateur in spectateurs2:
         pres12.append(spectateur)
 
-    for spectateur in pres12:
+    # remplir la liste avec les noms des spectateurs présents dans les salles 1, 2 et 3
+    for spectateur in spectateurs1:
+        pres123.append(spectateur)
+    for spectateur in spectateurs2:
         pres123.append(spectateur)
     for spectateur in spectateurs3:
         pres123.append(spectateur)
+
+    # ==================== Tests ====================
+
+    print("Spectateurs présents dans la salle 1 :")
+    for spectateur in spectateurs1:
+        print(spectateur)
+
+    print("\nSpectateurs présents dans la salle 2 :")
+    for spectateur in spectateurs2:
+        print(spectateur)
+
+    print("\nSpectateurs présents dans la salle 3 :")
+    for spectateur in spectateurs3:
+        print(spectateur)
+
+    # print("\nSpectateurs présents dans les deux premiers salles :")
+    # for spectateur in pres12:
+    #     print(spectateur)
+
+    # Afficher le nombre de spectateurs présents dans les salles 1 et 2
+    # print("Nombre de spectateurs présents dans les salles 1 et 2 : " + str(len(pres12)))
+    # Afficher le nombre de spectateurs présents dans les salles 1, 2 et 3
+    # print("Nombre de spectateurs présents dans les salles 1, 2 et 3 : " + str(len(pres123)))
+
+    # ==================== Tests ====================
 
     print(set(spectateurs1) & set(spectateurs2) & set(spectateurs3) == set(pres123))
     distances = []
 
     for i in range(len(trajet)-1):
+        # Calcul de la distance entre deux points
         distances.append(((trajet[i][0]-trajet[i+1][0])**2+(trajet[i][1]-trajet[i+1][1])**2)**0.5)
     print(distances)
-    print("\nLa distance totale est de", sum(distances))
 
     parcouru = 0
     for i in distances:
         parcouru += i
-    # print(parcouru)
     print("\nLa distance totale est de", parcouru)
 
     for i in range(25, 140, 15):
-
-        print(f"\nPour une vitesse de {i}km/h, il aurait fallu {parcouru/i}h pour parcourir la distance totale")
+        print(f"Pour une vitesse de {i}km/h, il aurait fallu {round(parcouru/i, 2)}h pour parcourir la distance totale")
 
 
 if __name__ == "__main__":
--- enquete_m.py
+++ enquete_m.py
@@ -1,12 +1,16 @@
 import names
+import random
 
 salle1 = "Salle 1"
-spectateurs1 = [names.get_full_name() for i in range(15)]
+spectateurs1 = [names.get_last_name() for i in range(random.randint(5, 20))]
 
 salle2 = "Salle 2"
-spectateurs2 = [names.get_full_name() for i in range(10)]
+spectateurs2 = [names.get_last_name() for i in range(random.randint(5, 20))]
+spectateurs2.append(spectateurs1[4])
+
 
 salle3 = "Salle 3"
-spectateurs3 = [names.get_full_name() for i in range(20)]
+spectateurs3 = [names.get_last_name() for i in range(random.randint(5, 20))]
+spectateurs3.append(spectateurs1[4])
 
 trajet = [(91.68, 53.10), (81.52, 50.14), (43.68, 18.18)]
